e4659e15d0bef3ce235a484d6599e06f39080f1f
Aroosa <irsataj9@gmail.com>
2025-11-09 21:44:00 +0500

    add component

---
+++ app/components/Nav.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eius obcaecati delectus non quo officiis soluta molestias ea est saepe, laborum perferendis vero accusamus laboriosam adipisci libero laudantium. Incidunt, molestiae!</p>
+    
+    
+    </>
+  )
+}
+
+export default Nav
